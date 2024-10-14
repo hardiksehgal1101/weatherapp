fbb7c4b2899af853993df712bdd94c7a9e6b265e
Hardik <79451662+hardiksehgal1101@users.noreply.github.com>
2024-10-14 21:26:43 +0530

    bugfix alt

--- src/modules/CityComponent.js
+++ src/modules/CityComponent.js
@@ -6,18 +6,6 @@ import perfectday from "..//icons/main.gif"
 const CityComponent = (props) => {
   return (
     <>
-      {/*  */}
-      
-      {/* <form id="SearchBox" onSubmit={fetchWeather}>
-        <input 
-          onChange={(e) => updateCity(e.target.value)}
-          placeholder="Enter City"
-        />
-        <button type={"submit"}><lord-icon
-    src="https://cdn.lordicon.com/pvbutfdk.json"
-    trigger="hover">
-</lord-icon></button>
-      </form> */}
       <img alt="weatherlogo" id="WeatherLogo" src={perfectday} />
     </>
   );
